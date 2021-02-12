1f667fcf12a46885256f62e5d04bc4184be47290
Alex <76395998+Blckvia@users.noreply.github.com>
2021-02-12 21:33:20 +0500

    Add files via upload

---
+++ js/ryaba.js
@@ -0,0 +1,53 @@
+const var1Node = $('.cl_var1');
+const var2Node = $('.cl_var2');
+const var3Node = $('.cl_var3');
+const var4Node = $('.cl_var4');
+const var5Node = $('.cl_var5');
+const var6Node = $('.cl_var6');
+const speechNode = $('.cl_speech');
+
+const btnCreate = $('.btn-create');
+const btnReplace = $('.btn-replace');
+const textNode = $('.cl_text');
+
+const startObj = {
+    "text": [
+        "Жили-были {var1} да {var2}",
+        "Была у них {var3}",
+        "Снесла {var3} {var4}, не простое - золотое",
+        "- {var1} бил, бил - не разбил",
+        "- {var2} била, била - не разбила",
+        "{var5} бежала, {var6} задела, {var4} упало и разбилось.",
+        "{var1} плачет, {var2} плачет, а {var3} кудахчет:",
+        "{speech}"
+    ]
+};
+
+btnCreate.click(function() {
+    textNode.html(startObj.text);
+})
+
+btnReplace.click(function() {
+    const var1 = var1Node.val();
+    const var2 = var2Node.val();
+    const var3 = var3Node.val();
+    const var4 = var4Node.val();
+    const var5 = var5Node.val();
+    const var6 = var6Node.val();
+    const speech = speechNode.val();
+
+    const newObj = {
+        "text": [
+            `Жили-были ${var1} да ${var2}`,
+            `Была у них ${var3}`,
+            `Снесла ${var3} ${var4}, не простое - золотое`,
+            `- ${var1} бил, бил - не разбил`,
+            `- ${var2} била, била - не разбила`,
+            `${var5} бежала, ${var6} задела, ${var4} упало и разбилось.`,
+            `${var1} плачет, ${var2} плачет, а ${var3} кудахчет:`,
+            `${speech}`
+        ]
+    };
+
+    textNode.html(newObj.text);
+})
